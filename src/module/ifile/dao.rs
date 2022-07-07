7f30c293e4bbb3700f55694b968e57b9dea059d0
xuguangyuansh <xuguangyuansh@126.com>
2022-07-07 19:05:17 +0800

    Update dao.rs

--- src/module/ifile/dao.rs
+++ src/module/ifile/dao.rs
@@ -35,7 +35,7 @@ pub async fn delete(path: &str) -> u64 {
 }
 
 pub async fn delete_all(path: &str) -> u64 {
-    RB.remove_by_wrapper(
+    RB.remove_by_wrapper::<Files>(
         RB.new_wrapper().like("path", format!("{}{}{}", path, path::MAIN_SEPARATOR, "%"))
     ).await.unwrap()
 }
