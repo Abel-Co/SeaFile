d9275f817be5be654153f21f11175fdae397bae2
xuguangyuansh <xuguangyuansh@126.com>
2022-07-18 06:43:02 +0800

    feat(*): 1.巡视功能开发中；

--- src/module/filesystem/async_patrol_sweep.rs
+++ src/module/filesystem/async_patrol_sweep.rs
@@ -1,19 +1,25 @@
+use std::path::Path;
 use futures::{
     channel::mpsc::channel,
     SinkExt, StreamExt,
 };
+use crate::module::ifile;
 
 use crate::module::ifile::Files;
 
 pub async fn async_patrol(_files: Vec<Files>) {
-    tokio::sync::mpsc::channel()
+
     let (mut tx, rx) = channel(1 << 16); // 65536*2
     tx.send(_files).await.unwrap();
 }
 
 pub async fn async_watch() {
-    let (mut tx, mut rx) = channel::<Files>(1 << 16); // 65536*2
-    while let Some(event) = rx.next().await {
-        log::info!("{:?}", event)
+    let (mut tx, mut rx) = channel::<Vec<Files>>(1 << 16); // 65536*2
+    while let Some(files) = rx.next().await {
+        for _file in files {
+            if !Path::new(_file.path.as_str()).exists() {
+                ifile::dao::delete_by_path(_file.path.as_str()).await
+            }
+        }
     }
 }
