8cdafc4d2c6c1df7f9fd0667674dcafced2b6968
xuguangyuansh <xuguangyuansh@126.com>
2023-01-04 18:40:38 +0800

    debug(*): Command smb account；

--- src/module/init.rs
+++ src/module/init.rs
@@ -39,11 +39,10 @@ pub async fn init_smb_account() {
         if !String::from_utf8_lossy(&output.unwrap().stdout).contains("Alpine Linux") {return}
         for user in RB.fetch_list::<Users>().await.unwrap() {
             let account = user.username.unwrap();
-            let output = Command::new("adduser").arg("-D").arg(&account).output();
-            log::info!("adduser: {} - {}", &account, String::from_utf8_lossy(&output.unwrap().stdout).to_string());
+            let _ = Command::new("adduser").arg("-D").arg(&account).output();
             if UserType::User == user.user_type {
                 let password = user.password.unwrap();
-                let double_passwd = format!("{}\\n{}\\n", password, password);
+                let double_passwd = format!("{}\n{}\n", password, password);
                 let output = Command::new("echo -e").arg(double_passwd).arg(" | smbpasswd -a -s").arg(&account).output();
                 log::info!("{}", String::from_utf8_lossy(&output.unwrap().stdout).to_string());
             }
