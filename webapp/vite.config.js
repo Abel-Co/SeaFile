e85a1db2ec075108a20dfac7f9d810ef9d49e524
xuguangyuansh <xuguangyuansh@126.com>
2023-02-08 17:59:03 +0800

    feat(*): 调试cdn。

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -10,26 +10,27 @@ export default defineConfig(({ command, mode }) => {
   let viteConfig = {
     plugins: [vue(), Components({ resolvers: [NaiveUiResolver()] })],
     build: { outDir: '../dist' },
-    resolve: {
-      alias: {
-        // 'vue': 'https://cdn.jsdelivr.net/npm/vue@3.2.47/+esm',
-        'vue': 'https://cdn.bytedance.com/cdn/vue/3.2.31/vue.esm-browser.prod.min.js',
-        // 'vue-router': 'https://cdn.jsdelivr.net/npm/vue-router@4.1.6/+esm',  // 不可用：Cannot read properties of undefined (reading 'value')
-        'axios': 'https://cdn.jsdelivr.net/npm/axios@1.3.1/+esm',
-        'json-bigint': 'https://cdn.jsdelivr.net/npm/json-bigint@1.0.0/+esm',
-        '_': 'https://cdn.jsdelivr.net/npm/lodash@4.17.21/+esm',
-        // 'vee-validate': 'https://cdn.jsdelivr.net/npm/vee-validate@4.7.3/+esm',  // 不可用：不报错，不出结果
-        // 'naive-ui': 'https://cdn.jsdelivr.net/npm/naive-ui@2.34.3/+esm',          // 不可用：不报错，页面不显示
-        'vditor': 'https://cdn.jsdelivr.net/npm/vditor@3.9.0/+esm',
-        // 'vue3-promise-dialog': 'https://cdn.jsdelivr.net/npm/vue3-promise-dialog@0.3.4/+esm',  // 不可用：组件所在页面失败
-        // 'vue3-video-play': 'https://cdn.jsdelivr.net/npm/vue3-video-play@1.3.1-beta.6/+esm'    // 不可用：组件所在页面失败
-      }
-    }
   }
   if (command === 'build') {  // 情景配置
     const env = loadEnv(mode, process.cwd(), '')
     viteConfig.base = '/'   // env.BASE    // github.io/<REPO>/
     viteConfig.define = { __APP_ENV__: mode }
+    viteConfig.resolve = {
+      alias: {
+        // 'vue': 'https://cdn.jsdelivr.net/npm/vue@3.2.47/+esm',
+        // 'vue': 'https://cdn.bytedance.com/cdn/vue/3.2.31/vue.esm-browser.prod.min.js',
+        'vue': 'https://cdn.bytedance.com/cdn/vue/3.2.31/vue.runtime.esm-browser.prod.min.js',
+        // 'vue-router': 'https://cdn.jsdelivr.net/npm/vue-router@4.1.6/+esm',      // 失败：Cannot read properties of undefined (reading 'value')
+        'axios': 'https://cdn.jsdelivr.net/npm/axios@1.3.1/+esm',
+        'json-bigint': 'https://cdn.jsdelivr.net/npm/json-bigint@1.0.0/+esm',
+        '_': 'https://cdn.jsdelivr.net/npm/lodash@4.17.21/+esm',
+        // 'vee-validate': 'https://cdn.jsdelivr.net/npm/vee-validate@4.7.3/+esm',  // 失败：不报错，不出结果
+        // 'naive-ui': 'https://cdn.jsdelivr.net/npm/naive-ui@2.34.3/+esm',         // 失败：不报错，页面不显示
+        // 'vditor': 'https://cdn.jsdelivr.net/npm/vditor@3.9.0/+esm',              // 失败：Failed to fetch dynamically imported module
+        // 'vue3-promise-dialog': 'https://cdn.jsdelivr.net/npm/vue3-promise-dialog@0.3.4/+esm',  // 不可用：组件所在页面失败
+        // 'vue3-video-play': 'https://cdn.jsdelivr.net/npm/vue3-video-play@1.3.1-beta.6/+esm'    // 不可用：组件所在页面失败
+      }
+    }
     viteConfig.plugins.push(
       /*visualizer({
         gzipSize: true,
