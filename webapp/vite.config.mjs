1d5350d3b918aeb7247f57d8fff0092a412b0d48
xuguangyuansh <xuguangyuansh@126.com>
2024-03-10 12:22:24 +0800

    build(*):     1.停用CDN资源；     2.

--- webapp/vite.config.mjs
+++ webapp/vite.config.mjs
@@ -22,19 +22,19 @@ export default defineConfig(({ command, mode }) => {
     viteConfig.define = { __APP_ENV__: mode }
     viteConfig.resolve = {
       alias: {
-        // 'vue': 'https://cdn.jsdelivr.net/npm/vue@3.2.47/+esm',
-        // 'vue': 'https://cdn.bytedance.com/cdn/vue/3.2.31/vue.esm-browser.prod.min.js',
-        'vue': 'https://cdn.bytedance.com/cdn/vue/3.4.21/vue.runtime.esm-browser.prod.min.js',
-        // 'vue-router': 'https://cdn.jsdelivr.net/npm/vue-router@4.1.6/+esm',      // 失败：未完全+esm. Cannot read properties of undefined (reading 'value')
-        // 'axios': 'https://cdn.jsdelivr.net/npm/axios@1.3.1/+esm',
-        'axios': 'https://cdn.staticfile.org/axios/1.6.7/esm/axios.min.js',
-        'json-bigint': 'https://cdn.jsdelivr.net/npm/json-bigint@1.0.0/+esm',
-        '_': 'https://cdn.jsdelivr.net/npm/lodash@4.17.21/+esm',
-        // 'vee-validate': 'https://cdn.jsdelivr.net/npm/vee-validate@4.7.3/+esm',  // 失败：不报错，不出结果
-        // 'naive-ui': 'https://cdn.jsdelivr.net/npm/naive-ui@2.34.3/+esm',         // 失败：不报错，页面不显示
-        // 'vditor': 'https://cdn.jsdelivr.net/npm/vditor@3.9.0/+esm',              // 失败：Failed to fetch dynamically imported module
-        // 'vue3-promise-dialog': 'https://cdn.jsdelivr.net/npm/vue3-promise-dialog@0.3.4/+esm',  // 不可用：组件所在页面失败
-        // 'vue3-video-play': 'https://cdn.jsdelivr.net/npm/vue3-video-play@1.3.1-beta.6/+esm'    // 不可用：组件所在页面失败
+        // // 'vue': 'https://cdn.jsdelivr.net/npm/vue@3.2.47/+esm',
+        // // 'vue': 'https://cdn.bytedance.com/cdn/vue/3.2.31/vue.esm-browser.prod.min.js',
+        // 'vue': 'https://cdn.bytedance.com/cdn/vue/3.4.21/vue.runtime.esm-browser.prod.min.js',
+        // // 'vue-router': 'https://cdn.jsdelivr.net/npm/vue-router@4.1.6/+esm',      // 失败：未完全+esm. Cannot read properties of undefined (reading 'value')
+        // // 'axios': 'https://cdn.jsdelivr.net/npm/axios@1.3.1/+esm',
+        // 'axios': 'https://cdn.staticfile.org/axios/1.6.7/esm/axios.min.js',
+        // 'json-bigint': 'https://cdn.jsdelivr.net/npm/json-bigint@1.0.0/+esm',
+        // '_': 'https://cdn.jsdelivr.net/npm/lodash@4.17.21/+esm',
+        // // 'vee-validate': 'https://cdn.jsdelivr.net/npm/vee-validate@4.7.3/+esm',  // 失败：不报错，不出结果
+        // // 'naive-ui': 'https://cdn.jsdelivr.net/npm/naive-ui@2.34.3/+esm',         // 失败：不报错，页面不显示
+        // // 'vditor': 'https://cdn.jsdelivr.net/npm/vditor@3.9.0/+esm',              // 失败：Failed to fetch dynamically imported module
+        // // 'vue3-promise-dialog': 'https://cdn.jsdelivr.net/npm/vue3-promise-dialog@0.3.4/+esm',  // 不可用：组件所在页面失败
+        // // 'vue3-video-play': 'https://cdn.jsdelivr.net/npm/vue3-video-play@1.3.1-beta.6/+esm'    // 不可用：组件所在页面失败
       }
     }
     viteConfig.plugins.push(
